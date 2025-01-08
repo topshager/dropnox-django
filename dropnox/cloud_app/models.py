f1a8e974e6f6637e2524c00b12b0e21a72abd1b3
topshager <rem28777@gmail.com>
2025-01-08 22:09:00 +0200

    update models.py ,created class folder

--- dropnox/cloud_app/models.py
+++ dropnox/cloud_app/models.py
@@ -12,4 +12,17 @@ class User(AbstractUser):
 
     def self(self):
         return self.username
-    
+
+
+class Folder(models.Model):
+    folder_id = models.AutoField(primary_key=True)
+    name = models.TextField()
+    parent = models.ForeignKey(
+        'self',null=True ,blank=True,on_delete = models.CASCADE,related_name='subfolder'
+    )
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='folders')
+    type = models.CharField(max_length=255)
+    created_at = models.DateField(auto_now=True)
+    updated_at = models.DateField(auto_now=True)
+    def __str__(self):
+        return self.name
