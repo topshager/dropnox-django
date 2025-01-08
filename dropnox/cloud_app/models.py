27aa618905a7852c4c00479735eeb98485b0f26a
topshager <rem28777@gmail.com>
2025-01-08 22:15:18 +0200

    update models.py created folder class

--- dropnox/cloud_app/models.py
+++ dropnox/cloud_app/models.py
@@ -10,7 +10,7 @@ class User(AbstractUser):
     REQUIRED_FIELDS = [username,password]
     USERNAME_FIELD = 'username'
 
-    def self(self):
+    def __str__(self):
         return self.username
 
 
@@ -24,5 +24,21 @@ class Folder(models.Model):
     type = models.CharField(max_length=255)
     created_at = models.DateField(auto_now=True)
     updated_at = models.DateField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+
+class folder(models.Model):
+    file_id = models.AutoField(primary_key=True)
+    name = models.TextField()
+    folder = models.ForeignKey(
+        Folder,null=True,blank=True ,on_delete=models.CASCADE,related_name='files'
+    )
+    type = models.CharField(max_length=255)
+    content = models.BinaryField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name='files')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
     def __str__(self):
         return self.name
