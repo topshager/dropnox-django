65362610858333945d262b2d1c85909e1345757e
topshager <rem28777@gmail.com>
2025-01-08 22:03:19 +0200

    create user model

--- dropnox/cloud_app/models.py
+++ dropnox/cloud_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=77, unique=True)
+    password = models.CharField(max_length=55)
+    REQUIRED_FIELDS = [username,password]
+    USERNAME_FIELD = 'username'
+
+    def self(self):
+        return self.username
+    
