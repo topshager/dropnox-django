c50f91e75c891966f15d2bba3851507904347c32
topshager <rem28777@gmail.com>
2025-01-09 20:53:41 +0200

    create views home, login_view

--- dropnox/cloud_app/views.py
+++ dropnox/cloud_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    template = loader.get_template("hompage/home.html")
+    return HttpResponse(template.render(request))
+
+def login_view(request):
+    template = loader.get_template("auth/login.html")
+    return HttpResponse(template.render(request))
