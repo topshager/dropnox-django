627c4803e9226776163e6e21222991e71e0b4ea3
topshager <rem28777@gmail.com>
2025-01-10 23:39:07 +0200

    update urls.py added login path

--- dropnox/dropnox/urls.py
+++ dropnox/dropnox/urls.py
@@ -21,6 +21,7 @@ from cloud_app import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home,name='home'),
-    path('auth/login/', views.login_view ,name='login'),
+    path('login/', views.login_view ,name='login'),
+    path("register/",views.register_view ,name='register'),
     path('folder', views.folder_view,name='folder')
 ]
