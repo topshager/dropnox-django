3b8acd5efd1f9131afe8746b557e6f485eaba9b6
topshager <rem28777@gmail.com>
2025-01-10 23:08:25 +0200

    update DIRS in settings.py

--- dropnox/dropnox/settings.py
+++ dropnox/dropnox/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'cloud_app'
+    'cloud_app',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = "dropnox.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
