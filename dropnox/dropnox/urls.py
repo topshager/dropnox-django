76d0442d8fa0f3495d99c68ca2706a7218da525e
topshager <rem28777@gmail.com>
2025-01-10 23:26:09 +0200

    update urls

--- dropnox/dropnox/urls.py
+++ dropnox/dropnox/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home,name='home'),
     path('auth/login/', views.login_view ,name='login'),
+    path('folder', views.folder_view,name='folder')
 ]
