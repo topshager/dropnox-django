25886d76366cd139d15ccc4f582935917018b10e
topshager <rem28777@gmail.com>
2025-01-10 23:08:06 +0200

    update view.py

--- dropnox/cloud_app/views.py
+++ dropnox/cloud_app/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render
-from django.template import loader
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    template = loader.get_template("hompage/home.html")
-    return HttpResponse(template.render(request))
+    return render(request, 'cloud_app/home.html')  # No need for a context if not passing data
 
 def login_view(request):
-    template = loader.get_template("auth/login.html")
-    return HttpResponse(template.render(request))
+    return render(request, 'auth/login.html')
