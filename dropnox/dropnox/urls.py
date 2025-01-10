7e0eb268ce9fd6cf24462f5a8cae37ade7513858
topshager <rem28777@gmail.com>
2025-01-10 23:08:39 +0200

    inserted views in urls.py

--- dropnox/dropnox/urls.py
+++ dropnox/dropnox/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from cloud_app import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", views.home,name='home'),
+    path('auth/login/', views.login_view ,name='login'),
 ]
