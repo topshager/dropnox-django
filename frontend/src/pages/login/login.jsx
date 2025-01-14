faea941ed038c47d9a7ab737506554a1371eca15
topshager <rem28777@gmail.com>
2025-01-14 16:24:33 +0200

    bug fix - frontend using correct port for backend

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -11,7 +11,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post('api/auth/', {
+      const response = await axios.post('http://127.0.0.1:8000/api/auth/', {
         username,
         password,
       });
