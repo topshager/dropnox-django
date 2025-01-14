664b4ca29f0ab23af8bd290b62b20c1f27763138
topshager <rem28777@gmail.com>
2025-01-15 00:57:05 +0200

    added routing  in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/login/login";
+import Register from "./pages/register/register";
 
 function App() {
   return (
     <Router>
+            <div>
+              <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+          </ul>
+        </nav>
     <Routes>
       <Route path="/" element={<Login />} />
+      <Route path="/register" element={<Register />}/>
     </Routes>
+    </div>
   </Router>
   );
 };
