af832c63e99a6e06b4796b926e2aee7f5e4f2acc
topshager <rem28777@gmail.com>
2025-01-15 23:58:39 +0200

    update view.py

--- dropnox/cloud_app/views.py
+++ dropnox/cloud_app/views.py
@@ -21,13 +21,15 @@ def user_auth(request):
 def home(request):
     return render(request, 'cloud_app/home.html')  # No need for a context if not passing data
 
-def login_view(request):
-    return render(request, 'auth/login.html')
+def user_register(request):
+    if request.method == 'POST':
+         data = request.data
+         username = data.get('username')
 
-def register_view(request):
-    return render(request,'auth/register.html')
 
-def folder_view(request):
-    return render(request, 'cloud_app/folder.html')
-def upload_view(request):
-    return render(request,'cloud_app/upload.html')
+
+#def folder_view(request):
+#    return render(request, 'cloud_app/folder.html')
+#def upload_view(request):
+#    return render(request,'cloud_app/upload.html')
+#
