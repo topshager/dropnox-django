242045cbaed790e855a68051847add2d8fd9b1a4
topshager <rem28777@gmail.com>
2025-01-16 22:41:31 +0200

    update home.jsx

---
+++ frontend/src/pages/home/home.jsx
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Home(){
+  const [folders,setFolders] = useState([]);
+  const [files,setFiles] = useState([])
+}
+export default Home;
