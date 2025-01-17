410cb718c134771f211093498ae1a28f5407397f
topshager <rem28777@gmail.com>
2025-01-17 17:15:43 +0200

    commenting out  unregistred urls

--- dropnox/dropnox/urls.py
+++ dropnox/dropnox/urls.py
@@ -5,10 +5,9 @@ from cloud_app import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home,name='home'),
-    path('login/', views.login_view ,name='login'),
-    path('upload',views.upload_view,name='upload'),
-    path("register/",views.register_view ,name='register'),
-    path('folder', views.folder_view,name='folder'),
+    #path('upload',views.upload_view,name='upload'),
+    #path("register/",views.register_view ,name='register'),
+    #path('folder', views.folder_view,name='folder'),
     path('api/auth/',include('cloud_app.urls')),
     path('api/register/',include('cloud_app.urls')),
     path('api/home/',include('cloud_app.urls')),
