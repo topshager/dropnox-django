d4f98a5dbdae4dbe1bd2823148f473e87c4d73be
topshager <rem28777@gmail.com>
2025-01-17 16:23:43 +0200

    update home.jsx

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -3,6 +3,65 @@ import axios from 'axios';
 
 function Home(){
   const [folders,setFolders] = useState([]);
-  const [files,setFiles] = useState([])
+  const [files,setFiles] = useState([]);
+  const [loading,setloading] = useState(true);
+  const [error,setError] = useState(null);
+
+
+  useEffect(() => {
+    fetch('api/home/',{
+      method:'GET',
+      credentials:'include',
+    })
+    .then((response)=>{
+      if(!response.ok){
+        throw new Error('Failed to fetch data');
+      }
+      return response.json();
+    })
+    .then((data) =>{
+      const [foldersData,filesData] = data.data;
+      setFolders(foldersData);
+      setFiles(filesData);
+      setloading(false);
+    })
+    .catch((error) =>{
+      console.error('error',error);
+      setError(error.message);
+      setloading(false);
+    });
+  },[]);
+  if (loading)return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <div>
+        <h2>Folders</h2>
+        {folders.length > 0 ? (
+          <ul>
+            {folders.map((folder) => (
+              <li key={folder.id}>{folder.name}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>No folders available.</p>
+        )}
+      </div>
+      <div>
+        <h2>Files</h2>
+        {files.length > 0 ? (
+          <ul>
+            {files.map((file) => (
+              <li key={file.id}>{file.name}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>No files available.</p>
+        )}
+      </div>
+    </div>
+  );
 }
 export default Home;
