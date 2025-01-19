95c64f6083b65e3ae563f9df82f83ded231093ce
topshager <rem28777@gmail.com>
2025-01-19 21:46:25 +0200

    update view,py

--- dropnox/cloud_app/views.py
+++ dropnox/cloud_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -8,9 +9,9 @@ from cloud_app.models import User,Folder,File
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from django.contrib.auth import authenticate
-
-
 from django.http import JsonResponse
+
+
 class MyTokenObtainPairView(TokenObtainPairView):
     pass
 
@@ -20,8 +21,6 @@ class MyTokenRefreshView(TokenRefreshView):
 
 @api_view(['POST'])
 def user_auth(request):
-    if request.method == 'POST':
-
         data = request.data
         username = data.get('username')
         password = data.get('password')
@@ -31,7 +30,6 @@ def user_auth(request):
 
         user = authenticate(username= username,password = password)
         if user is not None:
-
             login(request, user)
             user_id = user.id  # Retrieve the user's ID
             return JsonResponse({
@@ -61,21 +59,19 @@ def home(request):
 
 @api_view(['POST'])
 def user_register(request):
-    if request.method == 'POST':
          data = request.data
          username = data.get('username')
          password = data.get('password')
-         user = authenticate(username= username)
-         if  not user:
 
-             new_user = User(username = username,password = password)
-             new_user.save()
-         else:
+         if  not  username or not password:
+               return JsonResponse({"message":"Username and password are required"},status=400)
+         if User.objects.filter(username=username).exists():
             return JsonResponse({"message": "username is not available"}, status=400)
 
-#def folder_view(request):
-
-#    return render(request, 'cloud_app/folder.html')
-#def upload_view(request):
-#    return render(request,'cloud_app/upload.html')
-#
+         new_user = User(username=username,password=password)
+         new_user.save()
+         return JsonResponse({"message": "User registered successfully!"}, status=201)
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def protected_view(request):
+    return Response({"message": "You have access to this protected view!"})
