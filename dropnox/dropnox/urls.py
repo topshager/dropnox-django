3580f67203badf073d6a4724101ca9c2c3f6118a
topshager <rem28777@gmail.com>
2025-01-21 00:57:13 +0200

    update main level urls.py file

--- dropnox/dropnox/urls.py
+++ dropnox/dropnox/urls.py
@@ -5,12 +5,7 @@ from cloud_app import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home,name='home'),
-    #path('upload',views.upload_view,name='upload'),
-    #path("register/",views.register_view ,name='register'),
-    #path('folder', views.folder_view,name='folder'),
-    path('api/token/',include('cloud_app.urls')),
-    path('api/auth/',include('cloud_app.urls')),
+    path('api/',include('cloud_app.urls')),
     path('api/register/',include('cloud_app.urls')),
-    path('api/home/',include('cloud_app.urls')),
 
 ]
