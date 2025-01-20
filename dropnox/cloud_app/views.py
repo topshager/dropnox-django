ba98cf5da4d3b166d4aff03b353f8be825b3921b
topshager <rem28777@gmail.com>
2025-01-20 08:54:07 +0200

    token auth

--- dropnox/cloud_app/views.py
+++ dropnox/cloud_app/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
+import logging
+logger = logging.getLogger(__name__)
+
 from cloud_app.models import User,Folder,File
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 from django.contrib.auth import authenticate
 from django.http import JsonResponse
 
+class customTokenObtainPairSerializer(TokenObtainPairSerializer):
+     def validate(self,attrs):
+         data = super().validate(attrs)
+         user = self.user
+         data['user_id'] = user.id
+         data['username'] = user.username
+         return data
 
+
+
+MyTokenObtainPairView
 class MyTokenObtainPairView(TokenObtainPairView):
-    pass
-
+    serializer_class = customTokenObtainPairSerializer
+    def post(self,request,*args,**kwargs):
+         logger.info("Requested data:", request.data)
+         response = super().post(request, *args, **kwargs)
+         logger.info("Response data:", response.data)
+         return response
 
 class MyTokenRefreshView(TokenRefreshView):
     pass
@@ -23,6 +41,7 @@ class ProtectedView(APIView):
       permission_classes = [[IsAuthenticated]]
       def get(self, request):
         return Response({"message": "You have access to this protected view!"})
+
 @api_view(['POST'])
 def user_auth(request):
         data = request.data
