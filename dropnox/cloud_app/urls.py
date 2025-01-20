80afdff19b8b9a0f1ad1314327f74f8ec631c756
topshager <rem28777@gmail.com>
2025-01-21 00:56:57 +0200

    update app level urls.py

--- dropnox/cloud_app/urls.py
+++ dropnox/cloud_app/urls.py
@@ -1,23 +1,14 @@
 from django.contrib import admin
-from django.urls import include ,path
+from django.urls import path
 from .views import MyTokenObtainPairView, MyTokenRefreshView, ProtectedView
 from . import views
 from . import views
 
 urlpatterns = [
 
-    path('register',views.user_register ,name='register'),
-
-    path('home',views.home , name='home'),
-    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/register/',views.user_register ,name='register'),
+    path('home/',views.home , name='home'),
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
-    path('api/protected/', ProtectedView.as_view(), name='protected_view'),
-
-
-    #path('upload_folder',views.)
-    #path('create_folder',views.),
-    #path('drag_drop_folder',views.),
-
-    #path('upload_file',views.),
-    #path('drag_drop_file',views.)
+    path('protected/', ProtectedView.as_view(), name='protected_view'),
 ]
