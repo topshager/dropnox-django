cb171aa57896889fde7f6de6ae384ee790c9341e
topshager <rem28777@gmail.com>
2025-01-21 10:45:15 +0200

    adding dropdown menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,10 +4,21 @@ import Login from "./pages/login/login";
 import Register from "./pages/register/register";
 import Home from "./pages/home/home";
 
+
 function App() {
   return (
     <Router>
             <div>
+            <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Dropdown Button
+        </Dropdown.Toggle>
+    <Dropdown.Menu>
+      <Dropdown.Item href="#/action">file upload</Dropdown.Item>
+      <Dropdown.Item href="#/action"> folder upload</Dropdown.Item>
+      <Dropdown.Item href="#/action">New folder</Dropdown.Item>
+      </Dropdown.Menu>
+      </Dropdown>
               <nav>
           <ul>
             <li>
