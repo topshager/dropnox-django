1747d17d70593bd724ed5518fc6e84529c18ca38
topshager <rem28777@gmail.com>
2025-01-22 08:46:53 +0200

    routing changes

--- dropnox/dropnox/urls.py
+++ dropnox/dropnox/urls.py
@@ -5,5 +5,5 @@ from cloud_app import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.home,name='home'),
-    path('api/',include('cloud_app.urls'))
+    path('api/',include('cloud_app.urls')),
 ]
