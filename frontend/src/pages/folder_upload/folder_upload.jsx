7a5c32408256f7f4d61daf41e4e11cca9188e542
topshager <rem28777@gmail.com>
2025-01-25 23:43:30 +0200

    Update folder_upload.jsx

--- frontend/src/pages/folder_upload/folder_upload.jsx
+++ frontend/src/pages/folder_upload/folder_upload.jsx
@@ -5,8 +5,20 @@ import api from "../auth/auth";
 
 const upload_folder = ()=>{
 
+  const[folder,setFolder] = useState(null);
+  const[name,setName] = useState("");
+  const[type,setType] = useState("document");
+  const[ status,setStatus] = useState("initial");
+
+  const handleFileChange = (e) =>{
+    if (e.target.files && e.target.files[0]){
+      setStatus("initial");
+      setFolder(e.target.fiels[0]);
+    }
+  };
+  
   return(
-    <h1>helllo upload</h1>
+
   )
 }
 
