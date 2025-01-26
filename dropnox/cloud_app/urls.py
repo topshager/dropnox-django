246f579a8e6af406a3ec003c4b580a60fad17286
topshager <rem28777@gmail.com>
2025-01-26 23:19:06 +0200

    created url for folder upload

--- dropnox/cloud_app/urls.py
+++ dropnox/cloud_app/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
     path('protected/', ProtectedView.as_view(), name='protected_view'),
-    path('home_upload/', views.home_upload_file,name='home_upload'),
+    path('home_upload/', views.home_upload_file,name='upload file'),
+    path('upload_folder/',views.upload_folder,name='upload folder')
+
 ]
