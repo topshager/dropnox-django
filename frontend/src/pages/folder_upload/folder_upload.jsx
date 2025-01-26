5b586a6580331fcfdd7e3f7086ef5152dce311c5
topshager <rem28777@gmail.com>
2025-01-26 17:29:35 +0200

    update folder_upload.jsx

--- frontend/src/pages/folder_upload/folder_upload.jsx
+++ frontend/src/pages/folder_upload/folder_upload.jsx
@@ -10,16 +10,47 @@ const upload_folder = ()=>{
   const[type,setType] = useState("document");
   const[ status,setStatus] = useState("initial");
 
-  const handleFileChange = (e) =>{
-    if (e.target.files && e.target.files[0]){
+  const handleFolderChange = (e) =>{
+    if (e.target.folders && e.target.folders[0]){
       setStatus("initial");
-      setFolder(e.target.fiels[0]);
+      setFolder(e.target.folders[0]);
     }
   };
-
-  return(
-
-  
-}
+  const handleUpload = async () =>{
+    setStatus("upload");
+    const formData = new FormData();
+    formData.append("folder",folder);
+    formData.append("name",name);
+    const token = localStorage.getItem("access_token");
+    if (!token){
+      console.error("No token found in localStorage");
+      setStatus("fail");
+      return;
+    }
+    try{
+      const response = await fetch("http://127.0.0.1:8000/api/upload_folder/",{
+        method: "POST",
+        body: formData,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data);
+        setStatus("success");
+      } else {
+        const errorData = await response.json();
+        console.error("Upload failed:", errorData);
+        setStatus("fail");
+      }
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      setStatus("fail");
+    }
+   }else {
+    alert("Please fill in all required fields!");
+  }
+};
 
 export default upload_folder;
