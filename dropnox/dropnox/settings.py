de9f31b34b58a690cb7c78baf63bd451897da0fa
topshager <rem28777@gmail.com>
2025-01-29 18:08:13 +0200

    update refresh token lifetime

--- dropnox/dropnox/settings.py
+++ dropnox/dropnox/settings.py
@@ -153,7 +153,7 @@ from datetime import timedelta
 
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'ALGORITHM': 'HS256',
