fb34bf013accd72f3505f0561292f6d142fcc22f
topshager <rem28777@gmail.com>
2025-01-30 14:24:12 +0200

    Update login.jsx

--- frontend/src/pages/login/login.jsx
+++ frontend/src/pages/login/login.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React, {useState } from 'react';
 import './login.css';
 import { useNavigate } from 'react-router-dom';
 import api from "../auth/auth";
