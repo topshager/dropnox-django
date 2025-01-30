466af087c8752b8fdb84e0eb47636efe5e14daeb
topshager <rem28777@gmail.com>
2025-01-30 15:33:09 +0200

    import route Link functionality

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './home.css';
+import { Link } from "react-router-dom";
 
 function Home() {
   const [folders, setFolders] = useState([]);
