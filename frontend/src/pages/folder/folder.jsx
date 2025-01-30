cbbc89c796ba539b16aa5832c8715b7466e33a54
topshager <rem28777@gmail.com>
2025-01-30 23:03:42 +0200

    fixed token autherisation

--- frontend/src/pages/folder/folder.jsx
+++ frontend/src/pages/folder/folder.jsx
@@ -8,20 +8,21 @@ function  Folder(){
   const [files,setFiles] = useState([])
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  
-  const token = localStorage.getItem('access_token');
+
+
 
   useEffect(()=> {
 
+  const token = localStorage.getItem('access_token');
   const  fetchData =async () =>{
     try{
       const response = await fetch('http://127.0.0.1:8000/api/subfolder/',{
-        id,
-        header:{
+        method:'POST',
+        headers:{
           Authorization:`Bearer ${token}`,
         },
+        body:JSON.stringify({ id }), 
       });
-
       if (!response.ok){
         throw new Error(`HTTP error! status: ${response.status}`);
       }
