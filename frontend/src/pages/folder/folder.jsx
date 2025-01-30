9341204211c856a69b6088f9406cf093416e6076
topshager <rem28777@gmail.com>
2025-01-30 17:24:54 +0200

    getting folder id

--- frontend/src/pages/folder/folder.jsx
+++ frontend/src/pages/folder/folder.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
+import { useParams } from "react-router-dom";
 import './folder';
 
 function  Folder(){
-
+  const {id} = useParams();
+  return (
+    <div>
+      <h1>Folder ID: {id}</h1>
+    </div>
+  );
 };
 
 export default  Folder;
