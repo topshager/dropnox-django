c13651ca18d56fb841ecfbf4f7fe861328ad27a3
topshager <rem28777@gmail.com>
2025-01-30 21:27:48 +0200

    urls path view update

--- dropnox/cloud_app/urls.py
+++ dropnox/cloud_app/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('home/',views.home , name='home'),
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', MyTokenRefreshView.as_view(), name='token_refresh'),
-    path('subfolder',views.subfolder,name='subfolder')
+    path('subfolder/',views.subfolder,name='subfolder'),
     path('protected/', ProtectedView.as_view(), name='protected_view'),
     path('home_upload/', views.home_upload_file,name='upload file'),
-    path('upload_folder/',views.upload_folder,name='upload folder')
+    path('upload_folder/',views.upload_folder,name='upload folder'),
 
 ]
