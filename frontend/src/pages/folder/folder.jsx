faf82f264fe6bb1735e73b07bfb71d1d5d0209fd
topshager <rem28777@gmail.com>
2025-02-01 21:55:20 +0200

    update folder.jsx

--- frontend/src/pages/folder/folder.jsx
+++ frontend/src/pages/folder/folder.jsx
@@ -4,26 +4,21 @@ import './folder.css';
 import { Link } from "react-router-dom";
 /*convert to useing api from auth avoid use of authentication header */
 function  Folder(){
-  const {id} = useParams();
+  const {folder_id} = useParams();
   const [folders,setFolders] = useState([]);
   const [files,setFiles] = useState([])
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
+  localStorage.setItem("id",`${folder_id}`)
 
 
   useEffect(()=> {
-  if (!id){
-    let  id = 0;
-
-  }
-
-
 
   const token = localStorage.getItem('access_token');
   const  fetchData =async () =>{
     try{
-      const response = await fetch(`http://127.0.0.1:8000/api/subfolder/${id}`,{
+      const response = await fetch(`http://127.0.0.1:8000/api/subfolder/${folder_id}`,{
         method:'GET',
         headers:{
           Authorization:`Bearer ${token}`,
@@ -50,17 +45,10 @@ function  Folder(){
   };
    fetchData();
 
-if (loading) {
-  return <p>Loading...</p>
-}
-if (error){
-  return <p>Error</p>
-}
-
 },[]);
   return (
     <div>
-      <h1>Folder ID: {id}</h1>
+      <h1>Folder ID: {folder_id}</h1>
       <h2>Folders</h2>
       <ul>
       {folders.map((folder) => (
