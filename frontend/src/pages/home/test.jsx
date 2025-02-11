811d8092611f3969c97894eba4d6f48679beea71
topshager <rem28777@gmail.com>
2025-02-11 21:42:57 +0200

    crate test.jsx

---
+++ frontend/src/pages/home/test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ThreeDotMenu from "../threeDotMenu/threeDotMenu";
+
+function TestComponent() {
+  return (
+    <div>
+      <h2>Test ThreeDotMenu</h2>
+      <ThreeDotMenu />
+    </div>
+  );
+}
+
+export default TestComponent;
