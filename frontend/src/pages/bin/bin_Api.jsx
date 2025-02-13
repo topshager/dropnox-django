d5788c24f1a1b19dae757b3b0473ad6639f9b8ac
topshager <rem28777@gmail.com>
2025-02-13 10:22:51 +0200

    update bin_Api.jsx

--- frontend/src/pages/bin/bin_Api.jsx
+++ frontend/src/pages/bin/bin_Api.jsx
@@ -2,10 +2,10 @@ import { useEffect,useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Bin(){
-  const { id } = useParams();
-  <p>{id}</p>
+  const { fileId } = useParams();
+  console.log(`this is ${fileId}`)
   return(
-    <p>DELETE</p>
+    <p>DELETE {fileId}</p>
   );
 };
 
