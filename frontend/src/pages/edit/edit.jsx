33425f75b9fcf3030bc55f7e09fe002a47bf5a20
topshager <rem28777@gmail.com>
2025-02-14 20:54:39 +0200

    update edit.jsx

--- frontend/src/pages/edit/edit.jsx
+++ frontend/src/pages/edit/edit.jsx
@@ -1,4 +1,9 @@
-function Edit(){
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Edit({}){
+  const {ID} =useParams()
+  console.log(`Edit ID ${ID}`)
   return (
     <p>Edit page</p>
   )
