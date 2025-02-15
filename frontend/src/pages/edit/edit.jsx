74b2ead8e46e509212dfa2d25866af2d40f58296
topshager <rem28777@gmail.com>
2025-02-15 13:05:04 +0200

    edit.jsx updating the form

--- frontend/src/pages/edit/edit.jsx
+++ frontend/src/pages/edit/edit.jsx
@@ -1,13 +1,24 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Popup from 'reactjs-popup';
+import { useForm } from "react-hook-form";
+
+function Edit(){
+
+  const {register,handleSubmit,watch,formState:{errors}} = useForm();
+  const onSubmit = data =>console.log(data);
+  console.log(watch("example"));
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input defaultValue="text" {...register("example")}/>
 
 
-const MyPopup = () => (
-  <Popup trigger={<button>Open Popup</button>} position="right center">
-    <div>Popup content here!</div>
-  </Popup>
-);
+      {errors.exampleRequired && <span>This field is required</span>}
 
+      <input type="submit" />
+    </form>
+  );
+}
 
-export default MyPopup;
+export default Edit
