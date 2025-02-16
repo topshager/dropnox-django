3ac05b9b13fe51a0699a39089d9ca7c92584bf5e
topshager <rem28777@gmail.com>
2025-02-16 23:51:01 +0200

    update bin.jsx

--- frontend/src/pages/bin/bin.jsx
+++ frontend/src/pages/bin/bin.jsx
@@ -1,7 +1,11 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./bin.css";
-import { Dropdown } from 'react-bootstrap';
+
+// Menu imports
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 function Recycling_Bin() {
   const [folders, setFolders] = useState([]);
@@ -22,7 +26,7 @@ function Recycling_Bin() {
         });
 
         if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
+          throw new Error(`HTTP error! Status: ${response.status}`);
         }
 
         const data = await response.json();
@@ -54,6 +58,7 @@ function Recycling_Bin() {
           {folders.map((folder) => (
             <li key={folder.folder_id}>
               <Link to={`/folder/${folder.folder_id}`}>{folder.name}</Link>
+              <PositionedMenu />
             </li>
           ))}
         </ul>
@@ -65,13 +70,10 @@ function Recycling_Bin() {
           <p>No files found.</p>
         ) : (
           <ul>
-
             {files.map((file) => (
               <li key={file.file_id}>
-
                 <div className="File">
                   <p>{file.name}</p>
-
                   {file.file_url ? (
                     <a href={file.file_url} target="_blank" rel="noopener noreferrer">
                       Open File
@@ -89,5 +91,56 @@ function Recycling_Bin() {
   );
 }
 
+function PositionedMenu() {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  const handleRestore = () => {
+    setAnchorEl(null);
+  };
+  const handleDelete = () =>{
+
+  };
+
+  return (
+    <div>
+      <Button
+        id="demo-positioned-button"
+        aria-controls={open ? "demo-positioned-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+      >
+        Dashboard
+      </Button>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <MenuItem onClick={handleDelete}>Delete [X]</MenuItem>
+        <MenuItem onClick={handleRestore }>My account</MenuItem>
+
+      </Menu>
+    </div>
+  );
+}
 
 export default Recycling_Bin;
