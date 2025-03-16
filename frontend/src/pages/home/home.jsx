aa1b492659ca852f7f56381b2a21ca11db4b8f0a
topshager <rem28777@gmail.com>
2025-03-16 22:00:26 +0200

    update

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -20,6 +20,8 @@ function Home() {
 
   useEffect(() => {
     const token = localStorage.getItem("access_token");
+    const id = localStorage.setItem(0);
+
 
     const fetchData = async () => {
       try {
