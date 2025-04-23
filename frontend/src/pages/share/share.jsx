42bba62d02499609fdf3173480134b6aa86c0b3d
topshager <rem28777@gmail.com>
2025-04-23 18:07:43 +0200

    Update share.jsx

--- frontend/src/pages/share/share.jsx
+++ frontend/src/pages/share/share.jsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 
-const sharable = () =>{
+const Sharable = (id,type) =>{
+
+  const [error, setError] = useState(null);
+
+    useEffect(()=>{
+        const token = localStorage.getItem("access_token");
+
+        const fetchData = async () =>{
+            try{
+                const response = await fetch(`http://127.0.0.1:8000/api/sharable/${id,type}`,{
+                    headers: { Authorization: `Bearer ${token}` },
+                });
+                if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+            }catch (error) {
+                console.error("Error fetching data:", error);
+                setError(error.message || "Failed to fetch data");
+            }
+        }
+        fetchData();
+
+    },[id,type]);
+    
     return(
-        <h1>hello</h1>
+        <div className="sharable-code">
+            <form>
+                
+            </form>
+        </div>
     );
 }
 
 
-export default sharable
+export default Sharable;
