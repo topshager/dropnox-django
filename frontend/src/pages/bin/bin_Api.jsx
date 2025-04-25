5aba4b8cce959be59789476caf570447eef4476d
topshager <rem28777@gmail.com>
2025-04-25 09:42:23 +0200

    Update bin_Api.jsx

--- frontend/src/pages/bin/bin_Api.jsx
+++ frontend/src/pages/bin/bin_Api.jsx
@@ -1,71 +1,27 @@
-import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+const  deleteItem = async (ID, type) => {
+  const token = localStorage.getItem("access_token");
+  const formData = new FormData();
+  formData.append("type", type);
 
-function Bin() {
-  const {ID} = useParams();
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  console.log(`this is folder_id ${ID}`)
+  try {
+    const response = await fetch(`http://127.0.0.1:8000/api/bin_Api/${ID}`, {
+      method: "POST",
+      body: formData,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
 
-  useEffect(() => {
-    const token = localStorage.getItem("access_token");
-    if (!token) {
-      setError("No access token found.");
-      return;
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
 
+    const result = await response.json();
+    console.log("Success:", result);
 
-    const fetchData = async () => {
+  } catch (err) {
+    console.error("Error moving to bin:", err.message);
+  }
+};
 
-
-      const formData = new FormData()
-      const type = localStorage.getItem("Type");
-
-      if (type == "file"){
-        formData.append("type", "File");
-        console.log(`This  is file that i must delete ${ID}`)
-
-      }
-      else{
-        console.log(`This  is folder that i must delete ${ID}`)
-        formData.append("type", "Folder");
-
-
-      }
-
-
-
-
-
-      try {
-        const response = await fetch(`http://127.0.0.1:8000/api/bin_Api/${ID}`, {
-          method: "POST",
-          body:formData,
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status} ${response.statusText}`);
-        }
-
-        const result = await response.json();
-        setData(result);
-      } catch (err) {
-        setError(err.message);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return (
-    <div>
-      <h2>Bin Data</h2>
-      {error ? <p style={{ color: "red" }}>{error}</p> : <pre>{JSON.stringify(data, null, 2)}</pre>}
-    </div>
-  );
-}
-
-export default Bin;
+export default deleteItem;
