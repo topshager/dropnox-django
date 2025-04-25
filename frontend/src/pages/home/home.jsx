e7bcecfa61b5d1b4b6affff7c48aa9d0ab184d8e
topshager <rem28777@gmail.com>
2025-04-25 09:42:25 +0200

    Update home.jsx

--- frontend/src/pages/home/home.jsx
+++ frontend/src/pages/home/home.jsx
@@ -205,7 +205,7 @@ function ThreeDotMenu({ ID,type }) {
               </div>
             )}
           </Popup>
-          <Link to={`/bin_Api/${ID}`}>Delete</Link>
+          <button onClick={() => deleteItem(ID,type)}>Delete</button>
           <Link to={`/share/${ID}/${type}`}>Share</Link>
         </div>
       )}
@@ -219,5 +219,38 @@ ThreeDotMenu.propTypes = {
   ID: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
   type: PropTypes.string.isRequired,
 };
+
+const  deleteItem = async (ID, type) => {
+
+  const token = localStorage.getItem("access_token");
+
+  const formData = new FormData();
+  
+  formData.append("type", type);
+  console.log(formData.get("type"));
+
+
+
+  try {
+    const response = await fetch(`http://127.0.0.1:8000/api/bin_Api/${ID}`, {
+      method: "POST",
+      body: formData,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error(`Error: ${response.status} ${response.statusText}`);
+    }
+
+    const result = await response.json();
+    console.log("Success:", result);
+
+  } catch (err) {
+    console.error("Error moving to bin:", err.message);
+  }
+};
+
 
 export default Home;
