69aa5852d02c3e0c6826e090e22c1e4366421ec2
topshager <rem28777@gmail.com>
2025-04-26 10:58:47 +0200

    Update urls.py

--- dropnox/dropnox/urls.py
+++ dropnox/dropnox/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from django.urls import path,include
 from cloud_app import views
+from rest_framework.routers import DefaultRouter
+from cloud_app.views import FileViewSet
 
+router = DefaultRouter()
+router.register(r'files', FileViewSet, basename='file')
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home,name='home'),
-    path('api/',include('cloud_app.urls')),
+    path("", views.home, name='home'),
+    path('api/', include('cloud_app.urls')),
+    path('api/', include(router.urls)),  # ← include this line!
 ]
